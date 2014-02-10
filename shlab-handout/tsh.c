347b3defbde94ff058faf534cd55ea64e55b1e98
JonkiH[Agit config --global user.name JonkiH <jone11@ru.is>
2014-02-10 15:32:11 +0000

    Added from book to make eval work and the build in command quit

--- shlab-handout/tsh.c
+++ shlab-handout/tsh.c
@@ -173,6 +173,34 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS]; /* Argument list execve() */
+    char buf[MAXLINE];
+    int bg;
+    pid_t pid;
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL)
+	return;
+
+    if (!builtin_cmd(argv)) {
+        pid = fork();
+	if (pid == 0) {
+	    if (execve(argv[0], argv, environ) < 0) {
+	        printf("%s: Command not found. \n", argv[0]);
+	        exit(0);
+            }
+	}
+
+	if (!bg) {
+	    int status; 
+	    if (waitpid(pid, &status, 0) < 0)
+		unix_error("whitfg: waitpid error");
+	else
+	    printf("%d %s", pid, cmdline);
+	}
+    } 
+	
     return;
 }
 
@@ -239,6 +267,10 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if(!strcmp(argv[0], "quit"))
+	exit(0);
+    if(!strcmp(argv[0], "&"))
+	return 1;
     return 0;     /* not a builtin command */
 }
 
